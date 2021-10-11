4c723726fffcc69dbc731b1973a0eea7983673fc
Svetlin Nachev <svetlin.nachev@gmail.com>
2021-10-11 18:58:22 +0100

    Update movies.js

--- src/components/movies.js
+++ src/components/movies.js
@@ -4,7 +4,9 @@ import MovieItem from "./movieitem";
  class Movies extends Component{
 
     render() {
+            //Calling .map function to take the collection object and split the collection into seperate sections .  
             return this.props.movies.map((movie)=>{
+               //create a MovieItem per movie. Also passing data to movie item. Key is require to differenciate the different items
                return <MovieItem movie={movie} key={movie.imdbID}></MovieItem>
 
             })
