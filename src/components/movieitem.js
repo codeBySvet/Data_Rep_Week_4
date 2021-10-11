5d1e399014f0a7917fd063d0815d9c17ef25f4cd
Svetlin Nachev <svetlin.nachev@gmail.com>
2021-10-11 18:47:48 +0100

    Update movieitem.js

--- src/components/movieitem.js
+++ src/components/movieitem.js
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
 import Card from 'react-bootstrap/Card';
 
-
+{/* This class will auto generate components, per movie, based off the data that is referenced */}
 class MovieItem extends Component {
 
     render() {
         return (
             <div>
+                {/* Adding card styling from React Bootstrap */}
                 <Card>
+                     {/* Referencing the title property */}
                     <Card.Header>{this.props.movie.Title}</Card.Header>
                     <Card.Body>
                         <blockquote className="blockquote mb-0">
+                            {/* Referencing the image property */}
                             <img src={this.props.movie.Poster} width="200" height="200"></img>
                             <footer className="blockquote-footer">   
                             </footer>
+                            {/* Referencing the year property */}
                             {this.props.movie.Year}
                         </blockquote>
                     </Card.Body>
